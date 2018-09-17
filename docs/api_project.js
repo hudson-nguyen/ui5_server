221efd90d63238f3e1f0982ad155190424514795
Hudson Nguyen <hudson.nguyen@somotsoftvn.com>
2018-09-17 14:35:19 +0700

    Init source

---
+++ docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "express-rest-es2017-boilerplate",
+  "version": "1.2.2",
+  "description": "Express boilerplate for building RESTful APIs",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-09-17T07:32:18.069Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
